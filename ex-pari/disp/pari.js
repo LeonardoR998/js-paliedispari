6a87781143b0fefcbefa6ed2f39ecab690eebb5c
LeonardoR998 <leonardoricci839@gmail.com>
2024-10-18 15:12:46 +0200

    add function ex-2

--- ex-pari/disp/pari.js
+++ ex-pari/disp/pari.js
@@ -10,10 +10,18 @@
 // ! ELABORAZIONE //
 
 // Funzione per generare un numero casuale da 1 a 5
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 5) + 1;
+}
 
 // Funzione per determinare se un numero è pari o dispari
+function isEven(num) {
+  return num % 2 === 0;
+}
 
 // Chiediamo all'utente di scegliere pari o dispari e di inserire un numero
+const userChoice = prompt("Scegli 'pari' o 'dispari':").toLowerCase();
+const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5:"), 10);
 
 // Verifichiamo se il numero è valido
 
